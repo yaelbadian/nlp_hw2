eb6652a7b0edaa6f46292492f57b9d90f3a3c169
Nitay <nitay@campus.technion.ac.il>
2020-06-24 18:51:16 +0300

    finished part 2

--- main.py
+++ main.py
@@ -3,13 +3,29 @@ from torch.utils.data.dataloader import DataLoader
 from data_set import DepParserDataset
 from data_reader import DataMapping
 from model import DependencyParser
-from advanced_model import DependencyParser as advancedDependencyParser
+from advanced_model import DependencyParser as AdvancedDependencyParser
 import train_model
 
 path_train = "HW2-files/train.labeled"
 path_test = "HW2-files/test.labeled"
 paths_list = [path_train, path_test]
 
+
+
+# --------- training basic model ---------- #
+# data_mapping = DataMapping(paths_list)
+# word_vocab_size, pos_vocab_size = len(data_mapping.word_idx_mappings), len(data_mapping.pos_idx_mappings)
+# train = DepParserDataset(data_mapping, path_train)
+# train_loader = DataLoader(train, shuffle=True)
+# test = DepParserDataset(data_mapping, path_test, alpha_dropout=0)
+# test_loader = DataLoader(test, shuffle=False)
+#
+# for i in range(10):
+#   net = DependencyParser(word_vocab_size, pos_vocab_size)
+#   train_model.train(net, train_loader, test_loader, path='models/', epochs=10)
+
+
+# --------- training advanced model ---------- #
 vectors_strs = ["glove.6B.300d", "fasttext.en.300d", "fasttext.simple.300d", "glove.42B.300d",
                 "glove.840B.300d", "glove.twitter.27B.200d"]
 
@@ -20,16 +36,14 @@ hidden_dims = [0.3, 0.4, 0.5]
 mlp_dims = [0.3, 0.4, 0.5]
 
 for vectors_str in vectors_strs:
-    data_mapping = DataMapping(paths_list, vectors_str="")
+    data_mapping = DataMapping(paths_list, vectors_str=vectors_str)
     word_vocab_size, pos_vocab_size = len(data_mapping.word_idx_mappings), len(data_mapping.pos_idx_mappings)
-
     train = DepParserDataset(data_mapping, path_train)
     train_loader = DataLoader(train, shuffle=True)
     test = DepParserDataset(data_mapping, path_test, alpha_dropout=0)
     test_loader = DataLoader(test, shuffle=False)
     # net = DependencyParser(word_vocab_size, pos_vocab_size)
     word_embeddings = data_mapping.word_vectors
-
     word_embedding_dim = word_embeddings.shape[1]
     for lstm_dropout in lstm_dropouts:
         for lstm_layer in lstm_layers:
@@ -38,7 +52,7 @@ for vectors_str in vectors_strs:
                     for mlp in mlp_dims:
                         hidden_dim = int((word_embedding_dim + pos_emb_dim) * hidden)
                         mlp_dim = int(hidden_dim * 2 * mlp)
-                        net = advancedDependencyParser(word_embeddings, pos_vocab_size, pos_emb_dim, hidden_dim,
+                        net = AdvancedDependencyParser(word_embeddings, pos_vocab_size, pos_emb_dim, hidden_dim,
                                                        mlp_dim, lstm_layers, lstm_dropout)
                         path_str = f"models/{vectors_str.replace('.', '')}_{str(lstm_dropout).replace('.', '')}_{lstm_layer}_" \
                                    f"{pos_emb_dim}_{hidden_dim}_{mlp_dim}_"
