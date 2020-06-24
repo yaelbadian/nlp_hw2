4ca86719f0207ab6d25cdd97013c1c35fcc1d4a7
YaelBadian <yael@proteantecs.com>
2020-06-24 18:46:35 +0300

    fix

--- main.py
+++ main.py
@@ -13,18 +13,6 @@ paths_list = [path_train, path_test]
 vectors_strs = ["glove.6B.300d", "fasttext.en.300d", "fasttext.simple.300d", "glove.42B.300d",
                 "glove.840B.300d", "glove.twitter.27B.200d"]
 
-
-data_mapping = DataMapping(paths_list, vectors_str="")
-word_vocab_size, pos_vocab_size = len(data_mapping.word_idx_mappings), len(data_mapping.pos_idx_mappings)
-
-train = DepParserDataset(data_mapping, path_train)
-train_loader = DataLoader(train, shuffle=True)
-test = DepParserDataset(data_mapping, path_test, alpha_dropout=0)
-test_loader = DataLoader(test, shuffle=False)
-# net = DependencyParser(word_vocab_size, pos_vocab_size)
-word_embeddings = data_mapping.word_vectors
-
-word_embedding_dim = word_embeddings.shape[1]
 lstm_dropouts = [0, 0.1, 0.3]
 lstm_layers = [2, 3, 4]
 pos_emb_dims = [15, 25]
@@ -32,6 +20,17 @@ hidden_dims = [0.3, 0.4, 0.5]
 mlp_dims = [0.3, 0.4, 0.5]
 
 for vectors_str in vectors_strs:
+    data_mapping = DataMapping(paths_list, vectors_str="")
+    word_vocab_size, pos_vocab_size = len(data_mapping.word_idx_mappings), len(data_mapping.pos_idx_mappings)
+
+    train = DepParserDataset(data_mapping, path_train)
+    train_loader = DataLoader(train, shuffle=True)
+    test = DepParserDataset(data_mapping, path_test, alpha_dropout=0)
+    test_loader = DataLoader(test, shuffle=False)
+    # net = DependencyParser(word_vocab_size, pos_vocab_size)
+    word_embeddings = data_mapping.word_vectors
+
+    word_embedding_dim = word_embeddings.shape[1]
     for lstm_dropout in lstm_dropouts:
         for lstm_layer in lstm_layers:
             for pos_emb_dim in pos_emb_dims:
